499e2f8a16aee09ff0d5ab0e7dac806a198d6d79
Victor Rubinat <vrubinat@gmail.com>
2018-08-07 13:10:36 +0200

    add setCenter command google

---
+++ src/commands/google@latest/setCenter.js
@@ -0,0 +1,15 @@
+
+import Map from './../../core/RxMap';
+
+const setCenter = function (options) {
+  const { lat, lng, zoom } = options;
+  const map = this.getMap();
+  const lastValue = this.value();
+  const oldCenter = map.getCenter();
+  const center = { lat: lat || oldCenter.lat, lng: lng || oldCenter.lng };
+  map.setCenter(center);
+  map.setZoom(zoom || map.getZoom());
+  return lastValue;
+};
+
+Map.register('setCenter', setCenter);
