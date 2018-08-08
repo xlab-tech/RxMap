cb271b0df7746f9cde2259704e70ea211a3023e6
victor <vrubinat@gmail.com>
2018-08-08 16:29:31 +0200

    create config in diferent file

--- src/core/RxDynamicMap.js
+++ src/core/RxDynamicMap.js
@@ -1,14 +1,14 @@
 import { RxMap } from './RxMap';
 
-let Map;
+let _RxMap;
 
-const _commons = ['gps'];
+const _commons = ['gps', 'addData'];
 
 const createMap = () => {
-    if (!Map) {
-        Map = new RxDynamicMap();
+    if (!_RxMap) {
+        _RxMap = new RxDynamicMap();
     }
-    return Map;
+    return _RxMap;
 };
 const loadLib = (lib, type, name, version = 'latest') => {
     const _lib = _commons.includes(name) ? 'common' : lib;
@@ -17,27 +17,19 @@ const loadLib = (lib, type, name, version = 'latest') => {
 
 export class RxDynamicMap extends RxMap {
 
-    async load(id, conf) {
-        // conf =  {} || uid || http
-        const { type, commands, observers, config } = conf;
-        const load = await this.loadModules(type, commands, observers);
-        const { center, zoom } = config;
-        this.create(id, center.lat, center.lng, zoom);
-        return load;
-    }
-    async loadModules(lib, commands, observers) {
+    async load(lib, commands, observers, options = {}) {
         // First Load Map Lib
         if (lib === 'leaflet') {
             await import(/* webpackChunkName: "leaflet" */'leaflet');
         } else if (lib === 'google') {
             const { loadGoogle } = await import(/* webpackChunkName: "google" */'./../utils/google');
-            const google = await loadGoogle('AIzaSyCjj-I0sYedbWCAmAoW2LgAr4T2bkPa09Y');
+            const google = await loadGoogle(options.key);
         }
         // After Load all Componentes
-        const _commands = commands.map(key => loadLib(lib, 'commands', key));
-        const _observers = observers.map(key => loadLib(lib, 'observers', key));
+        const _commands = commands.map(key => loadLib(lib, 'commands', key, options.version));
+        const _observers = observers.map(key => loadLib(lib, 'observers', key, options.version));
         return Promise.all(_commands.concat(_observers))
-            .then(() => Map);
+            .then(() => this);
     }
     init() {
         return new RxDynamicMap();
---
+++ src/core/config.js
@@ -0,0 +1,32 @@
+import RxDynamicMap from './RxDynamicMap';
+import { take } from 'rxjs/internal/operators/take';
+
+const mandatoryCommands = ['create'];
+const mandatoryObservers = [];
+
+export const RxMapFromConfig = async (id, config) => {
+  const { type, commands, observers, map, options, dataTypes } = config;
+  const { center, zoom, autoCenter } = map;
+  const mapCenter = center || { lat: 0, lng: 0 };
+
+  let allComands = commands.concat(mandatoryCommands);
+  allComands = allComands.filter((item, pos) => allComands.indexOf(item) === pos);
+
+  let allObservers = observers.concat(mandatoryObservers, autoCenter ? 'gps' : []);
+  allObservers = allObservers.filter((item, pos) => allObservers.indexOf(item) === pos);
+
+  await RxDynamicMap.load(type, allComands, allObservers, options);
+
+  RxDynamicMap.create(id, mapCenter.lat, mapCenter.lng, zoom);
+  if (autoCenter) {
+    RxDynamicMap.observer('gps')
+      .pipe(take(1))
+      .setCenter(res => ({ lat: res.latitude, lng: res.longitude }))
+      .subscribe();
+  }
+  if (dataTypes) {
+    dataTypes.forEach(element => RxDynamicMap.setDataType(element.id, element.geomType, element.style));
+  }
+
+  return RxDynamicMap;
+};
