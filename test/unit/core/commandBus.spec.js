8ccff5899fc00402ce7e0344f3581a1701969c0d
victor <vrubinat@gmail.com>
2018-08-17 16:53:36 +0200

    fix test errors

--- test/unit/core/commandBus.spec.js
+++ test/unit/core/commandBus.spec.js
@@ -1,19 +1,18 @@
 
-import { beforeEach, afterEach, describe, it } from 'mocha';
+import {
+  describe, it,
+} from 'mocha';
 import { expect } from 'chai';
-
-import rMap from './../../../src/core/Map.js';
-import CommandBus from './../../../src/core/CommandBus.js';
+import CommandBus from '../../../src/core/CommandBus';
 
 describe('Create Map', () => {
-
-    it('create new Command Bus', () => {
-        const commandBus = new CommandBus();
-        expect(commandBus).to.be.an.instanceof(CommandBus);
-        const ret = commandBus.value();
-        expect(ret).to.deep.equal({
-            'value': null,
-            'name': null
-        });
+  it('create new Command Bus', () => {
+    const commandBus = new CommandBus();
+    expect(commandBus).to.be.an.instanceof(CommandBus);
+    const ret = commandBus.value();
+    expect(ret).to.deep.equal({
+      value: null,
+      name: null,
     });
+  });
 });
--- test/unit/core/register.spec.js
+++ test/unit/core/register.spec.js
@@ -1,48 +1,45 @@
 
-import { beforeEach, afterEach, describe, it } from 'mocha';
+import {
+  afterEach, describe, it,
+} from 'mocha';
 import { expect } from 'chai';
+import jsdom from 'mocha-jsdom';
+import { Observable } from 'rxjs/internal/Observable';
+import { of } from 'rxjs/internal/observable/of';
+import RxMap from '../../../src/RxMap';
+import CommandBus from '../../../src/core/CommandBus';
+import { registerCommand } from '../../../src/core/registerCommand';
+import { registerObserver } from '../../../src/core/registerObserver';
 
-import Map from './../../../src/core/Map.js';
-import CommandBus from '../../../src/core/CommandBus.js';
-import { Observable } from 'rxjs/Observable.js';
 
 describe('Register commands for execute', () => {
+  jsdom({
+    url: 'https://example.org/',
+  });
+  it('register command', () => {
+    expect(Observable).to.not.respondTo('testRegister');
+    expect(CommandBus).to.not.respondTo('testRegister');
 
-    it('register command', () => {
-        expect(Map).to.respondTo('register');
-        expect(Observable).to.not.respondTo('testRegister');
-        expect(CommandBus).to.not.respondTo('testRegister');
-        const func = function () {
-            return 'testRegister';
-        };
-        Map.register('testRegister', func);
-        expect(Observable).to.respondTo('testRegister');
-        expect(CommandBus).to.respondTo('testRegister');
-    });
-    it('register Observable', () => {
-        expect(Map).to.respondTo('registerObservable');
-        expect(CommandBus).to.respondTo('observer');
-        expect(Observable).to.respondTo('observer');
-        const func = function () {
-            return Observable.of(1);
-        };
-        Map.registerObservable('test', func);
-        const res = Map.observer('test');
-        const res2 = Observable.of(1).observer('test');
-        expect(res).to.instanceOf(Observable);
-        expect(res2).to.instanceOf(Observable);
+    registerCommand('testRegister', () => 'testRegister');
 
-    });
+    expect(Observable).to.respondTo('testRegister');
+    expect(CommandBus).to.respondTo('testRegister');
+  });
 
-    afterEach(() => {
-        delete Observable.prototype.testRegister;
-        delete CommandBus.prototype.testRegister;
-    });
-    
-    /*it('execute command', () => {
-        const ret = new CommandBus().test();
-        console.log('RET:',ret.value().value);
-        expect(true).to.be.true;
-    });*/
+  it('register Observable', () => {
+    expect(CommandBus).to.respondTo('observer');
+    expect(Observable).to.respondTo('observer');
+
+    registerObserver('test', () => of(1));
+
+    const res = RxMap.observer('test');
+    const res2 = of(1).observer('test');
+    expect(res).to.instanceOf(Observable);
+    expect(res2).to.instanceOf(Observable);
+  });
+
+  afterEach(() => {
+    delete Observable.prototype.testRegister;
+    delete CommandBus.prototype.testRegister;
+  });
 });
-
