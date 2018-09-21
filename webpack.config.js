b83db77bb051182d1ad57fe7aa8c4a1ee57da341
Victor Rubinat <vrubinat@gmail.com>
2018-09-21 16:14:50 +0200

    fix lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
